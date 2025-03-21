a2ddf599e790c1ab6099fd703949503c9a4d561b
Tejas Joshi <tejasjoshi9673@gmail.com>
2025-03-21 18:00:19 -0400

    Fix the test script

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-./bin/project3  tests/test1.bc > out.txt
+./bin/svf-project  tests/test1.bc > out.txt
 if [ -z $(grep "Reachable" out.txt) ]; then
     echo "test1 error"
 fi
 
-./bin/project3  tests/test2.bc > out.txt
+./bin/svf-project  tests/test2.bc > out.txt
 if [ -z $(grep "Reachable" out.txt) ]; then
     echo "test2 error"
 fi
